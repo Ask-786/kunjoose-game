be789ef8828a0a8a1d767aa46df7f1e1d83c4c15
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2024-12-29 20:59:34 +0530

    build

---
+++ build.sh
@@ -0,0 +1,11 @@
+if !command -v node >/dev/null 2>&1; then
+  echo "node is not installed"
+  exit 1
+fi
+
+if ! command -v pnpm >/dev/null 2>&1; then
+  npm install -g pnpm
+fi
+
+pnpm install
+pnpn run build
