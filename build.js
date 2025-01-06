320338a27e733248c25364f91531cd5d5e1fdb2a
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2025-01-06 22:08:07 +0530

    fix: update in build.js

--- build.js
+++ build.js
@@ -2,9 +2,21 @@
 const builder = require("electron-builder");
 const Platform = builder.Platform;
 
+const platform = process.argv[2];
+
+if (!platform) {
+  console.error("Platform not specified");
+  process.exit(1);
+}
+
+if (platform !== "WINDOWS" && platform !== "MAC" && platform !== "LINUX") {
+  console.error("Invalid platform, must be 'WINDOWS', 'MAC' or 'LINUX'");
+  process.exit(1);
+}
+
 builder
   .build({
-    targets: Platform.LINUX.createTarget(),
+    targets: Platform[platform].createTarget(),
     config: {
       productName: "Kunjoose Game",
       directories: { output: "dist/electron" },
